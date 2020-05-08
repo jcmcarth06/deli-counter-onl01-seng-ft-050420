30975fdea79d073394a34b40821570a1d5f3870a
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-08 00:48:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,10 @@ def line(people)
   if people.length == 0
     puts "The line is currently empty."
   else
-    puts people.join ". "
-    puts people.each |i|
+    numbered_people = []
+    people.each do |i|
+      numbered_people.push "#{people.index(i) + 1}. #{i} "
+    end
   end
 end
 
