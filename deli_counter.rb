15ec46a451516eafcec7f424afaad3577a88ced6
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-08 00:16:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,18 @@
 
 
-def line(array)
-  if array.length == 0
+def line(people)
+  if people.length == 0
     puts "The line is currently empty."
   end
+end
+
+def take_a_number(people, name)
+  if people.length > 0
+
+
+    puts "Welcome #{name}. You are number #{people.length + 1} in line."
+  end
+end
+
+def now_serving(people)
 end
